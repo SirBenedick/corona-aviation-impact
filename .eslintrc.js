98cc4a2767883c3dd150ee4ac8fa4babc65a38dc
unknown <nico-gensheimer@gmx.de>
2020-05-06 13:31:44 +0200

    initial project with vue, d3, element-ui

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { 
+    es6: true
+  }, 
+  extends: [
+    // add more generic rulesets here, such as:
+    //'eslint:recommended'
+    "plugin:vue/strongly-recommended"
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "indent": ["error", 2],
+    "newline-after-var": ["error", "always"],
+    "no-multiple-empty-lines": "error",
+    "no-console": ["warn", { allow: ["warn", "error"] }]
+  }
+}
