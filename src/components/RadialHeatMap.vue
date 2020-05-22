7d52272ac5d9de68b89a253423cdae6858aff230
SirBenedick <beneweb@outlook.de>
2020-05-22 16:01:18 +0200

    Legend etc...

--- src/components/RadialHeatMap.vue
+++ src/components/RadialHeatMap.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="arc" />
+  <div>
+    <div id="arc" />
+    <div id="legend" />
+  </div>
 </template>
 
 <script>
@@ -10,10 +13,10 @@ export default {
     return {
       radial_labels: ["January", "Feburary", "March", "April"],
       segment_labels: [
-        // {
-        //   countryCode: "TW",
-        //   countryDisplayedName: "Taiwan"
-        // },
+        {
+          countryCode: "TW",
+          countryDisplayedName: "Taiwan"
+        },
         {
           countryCode: "AR",
           countryDisplayedName: "Argentina"
@@ -70,10 +73,10 @@ export default {
           countryCode: "SA",
           countryDisplayedName: "Saudi Arabia"
         },
-        // {
-        //   countryCode: "ZA",
-        //   countryDisplayedName: "South Africa"
-        // },
+        {
+          countryCode: "ZA",
+          countryDisplayedName: "South Africa"
+        },
         {
           countryCode: "TR",
           countryDisplayedName: "Turkey"
@@ -214,7 +217,7 @@ export default {
         segmentLabels = [], // Value assigned only for init
         radialLabels = (segmentLabels = []); // Value assigned only for init
 
-      const freeSpaceSizeInPercent = 0.2;
+      const freeSpaceSizeInPercent = 0.18;
       const emptySpaceAngleInRad = freeSpaceSizeInPercent * 2 * Math.PI;
       const segementSapceAngleInRad = 2 * Math.PI - emptySpaceAngleInRad;
       const singleSegmentAngle = segementSapceAngleInRad / numSegments;
@@ -237,7 +240,7 @@ export default {
         );
       };
       let ea = function(d, i) {
-        if ((singleSegmentAngle * (i + 1)) % segementSapceAngleInRad === 0) {
+        if (Math.floor((singleSegmentAngle * (i + 1)) % segementSapceAngleInRad) === 0) {
           return 2 * Math.PI;
         }
         return (
@@ -275,11 +278,8 @@ export default {
             autoDomain = true;
           }
 
-          // Linear d3 color scale
-          var color = d3
-            .scaleQuantize()
-            .domain(domain)
-            .range(range);
+          // Color scale
+          var color = d3.scaleDiverging([-100, 0, 100], d3.interpolateRdBu);
 
           if (autoDomain) domain = null;
 
@@ -484,6 +484,150 @@ export default {
     },
     emitSelectCountry(newCountryName) {
       this.$emit("selectCountry", newCountryName);
+    },
+    ramp(color, n = 256) {
+      var canvas = document.createElement("CANVAS");
+      const context = canvas.getContext("2d");
+      for (let i = 0; i < n; ++i) {
+        context.fillStyle = color(i / (n - 1));
+        context.fillRect(i, 0, 100, 100);
+      }
+
+      return canvas;
+    },
+    legend({
+      color,
+      title,
+      tickSize = 6,
+      width = 320, 
+      height = 44 + tickSize,
+      marginTop = 18,
+      marginRight = 0,
+      marginBottom = 16 + tickSize,
+      marginLeft = 0,
+      ticks = width / 64,
+      tickFormat,
+      tickValues
+    } = {}) {
+
+      const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible")
+        .style("display", "block");
+
+      let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+      let x;
+
+      // Continuous
+      if (color.interpolate) {
+        const n = Math.min(color.domain().length, color.range().length);
+
+        x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+        svg.append("image")
+          .attr("x", marginLeft)
+          .attr("y", marginTop)
+          .attr("width", width - marginLeft - marginRight)
+          .attr("height", height - marginTop - marginBottom)
+          .attr("preserveAspectRatio", "none")
+          .attr("xlink:href", this.ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+      }
+
+      // Sequential
+      else if (color.interpolator) {
+        x = Object.assign(color.copy()
+          .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+        {range() { return [marginLeft, width - marginRight]; }});
+
+        svg.append("image")
+          .attr("x", marginLeft)
+          .attr("y", marginTop)
+          .attr("width", width - marginLeft - marginRight)
+          .attr("height", height - marginTop - marginBottom)
+          .attr("preserveAspectRatio", "none")
+          .attr("xlink:href", this.ramp(color.interpolator()).toDataURL());
+
+        // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+        if (!x.ticks) {
+          if (tickValues === undefined) {
+            const n = Math.round(ticks + 1);
+            tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+          }
+          if (typeof tickFormat !== "function") {
+            tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+          }
+        }
+      }
+
+      // Threshold
+      else if (color.invertExtent) {
+        const thresholds
+        = color.thresholds ? color.thresholds() // scaleQuantize
+          : color.quantiles ? color.quantiles() // scaleQuantile
+            : color.domain(); // scaleThreshold
+
+        const thresholdFormat
+        = tickFormat === undefined ? d => d
+          : typeof tickFormat === "string" ? d3.format(tickFormat)
+            : tickFormat;
+
+        x = d3.scaleLinear()
+          .domain([-1, color.range().length - 1])
+          .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+          .selectAll("rect")
+          .data(color.range())
+          .join("rect")
+          .attr("x", (d, i) => x(i - 1))
+          .attr("y", marginTop)
+          .attr("width", (d, i) => x(i) - x(i - 1))
+          .attr("height", height - marginTop - marginBottom)
+          .attr("fill", d => d);
+
+        tickValues = d3.range(thresholds.length);
+        tickFormat = i => thresholdFormat(thresholds[i], i);
+      }
+
+      // Ordinal
+      else {
+        x = d3.scaleBand()
+          .domain(color.domain())
+          .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+          .selectAll("rect")
+          .data(color.domain())
+          .join("rect")
+          .attr("x", x)
+          .attr("y", marginTop)
+          .attr("width", Math.max(0, x.bandwidth() - 1))
+          .attr("height", height - marginTop - marginBottom)
+          .attr("fill", color);
+
+        tickAdjust = () => {};
+      }
+
+      svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+          .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+          .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+          .tickSize(tickSize)
+          .tickValues(tickValues))
+        .call(tickAdjust)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+          .attr("x", marginLeft)
+          .attr("y", marginTop + marginBottom - height - 6)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .attr("font-weight", "bold")
+          .text(title));
+
+      return svg.node();
     }
   },
   mounted() {
@@ -495,6 +639,12 @@ export default {
       this.radial_labels,
       this.segment_labels
     );
+    let legend = this.legend({
+      color: d3.scaleDiverging([-1, 0, 1], d3.interpolateRdBu),
+      title: "Daily change",
+      tickFormat: "+%"
+    });
+    document.getElementById("legend").appendChild(legend)
   }
 };
 </script>
