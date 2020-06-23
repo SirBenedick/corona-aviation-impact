6bd15a7f539774d546070281388db706ea460d94
Alexander Köhler <alexander.koehler@osapiens.com>
2020-06-23 12:09:04 +0200

    Typos

--- src/components/CoronaLineChart.vue
+++ src/components/CoronaLineChart.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     Corona infections
-    <el-tooltip
-      effect="light"
-      placement="top"
-    >
+    <el-tooltip effect="light" placement="top">
       <div slot="content" style="width: 500px;">
-        The chart below shows the daily infection numbers for the selected country. 
-        It shows how the infection rate of COVID-19 has developed in 2020. The x-axis shows the weeks of the year 2020. The values are averaged using a 7-day window like the aviation traffic chart as well to account for weekly variations in the data due to authorities not reporting during weekends and to decrease the chance of indication wrong trends towards the end of the chart. <br/>
-        The case numbers are provided by the <a href="https://data.europa.eu/euodp/de/data/dataset/covid-19-coronavirus-data" target="_blank">EU Open Data Portal</a>.
+        The chart below shows the daily infection numbers for the selected country.
+        It shows how the infection rate of COVID-19 has developed in 2020. The x-axis shows the weeks of the year 2020. The values are averaged using a 7-day window like the aviation traffic chart as well to account for weekly variations in the data due to authorities not reporting during weekends and to decrease the chance of indicating wrong trends towards the end of the chart.
+        <br />The case numbers are provided by the
+        <a
+          href="https://data.europa.eu/euodp/de/data/dataset/covid-19-coronavirus-data"
+          target="_blank"
+        >EU Open Data Portal</a>.
       </div>
       <i class="el-icon-question" />
     </el-tooltip>
@@ -180,7 +181,7 @@ export default {
         .style("width", "150px")
         .style("text-align", "center")
         .style("background-color", "rgb(245,245,245,.8)")
-        .style("color","black")
+        .style("color", "black")
         .style("z-index", 10)
         .style("border", "solid")
         .style("border-width", "1px")
